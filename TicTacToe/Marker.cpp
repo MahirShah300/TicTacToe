6f2e6c6bf5a2305d0e49b2ffa69fa2800bd12004
Mahir Shah <mahir_300@hotmail.co.uk>
2024-09-24 22:29:26 +0100

    Added comments in Marker.cpp as plans for what to do next

--- TicTacToe/Marker.cpp
+++ TicTacToe/Marker.cpp
@@ -32,3 +32,12 @@ std::string Marker::getMark() const
 {
 	return mark;
 }
+
+// Make a player class that has it's own board and keeps record of where he chooses. if three in a row, he wins. 
+// Also make is so player has function that takes board as a reference and places mark, rather than board setting itself
+// Also maybe look into GUI
+player1_mark = BoardMarks.circle
+player1.mark(board = board, mark_type = player1_mark)
+{
+	board.take_mark(mark_type)
+}
